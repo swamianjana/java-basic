bbb524ecec020d08b2e6b6eb5b9fe2bc856c34a6
Anjana <anjana20swami@gmail.com>
2022-10-05 22:50:19 +0530

    (HashMap) LongestSubArrayWithSumK: impl. using hashmap.

--- src/HashMap/questions/LongestSubArrayWithSumK.java
+++ src/HashMap/questions/LongestSubArrayWithSumK.java
@@ -1,17 +1,21 @@
 package HashMap.questions;
 
+import java.util.HashMap;
+
 public class LongestSubArrayWithSumK {
 
 	public static void main(String[] args) {
 		int[] arr = {-5, 8, -14, 2, 4, 12};
 		
-		int res = longestSubArray(arr, -5);
+//		int res = longestSubArray(arr, -5);
+		int res = longestSubArrayWithMap(arr, -5);
 		System.out.println(res);
 	}
 	
+	// brute-force approach
 	static int longestSubArray(int[] arr, int k ) {
 		int longestSubArrayLength = 0;
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 0; i < arr.length; i++) { // n
 			int sum = 0;
 			for(int j = i; j < arr.length; j++) {
 				sum += arr[j]; //sum = sum + arr[j]; 
@@ -22,5 +26,32 @@ public class LongestSubArrayWithSumK {
 			}
 		}
 		return longestSubArrayLength;
+	}
+	
+	// optimize
+	static int longestSubArrayWithMap(int[] arr, int k) {
+		int longestSubArrLen = 0;
+		HashMap<Long, Integer> sumIndexMap = new HashMap<>();
+		
+		long sum = 0;
+		
+		sumIndexMap.put(0l, -1);
+		for(int i =0; i < arr.length; i++) {
+			sum += arr[i];
+			long searchKey = sum - k;
+			if(sumIndexMap.containsKey(searchKey)) {
+				int index = sumIndexMap.get(searchKey);
+				int len = i - index;
+				if(longestSubArrLen < len) {
+					longestSubArrLen = len;
+				}
+			}
+			if(!sumIndexMap.containsKey(sum)) {
+				sumIndexMap.put(sum, i);
+			}
+		}
+		
+		
+		return longestSubArrLen;
 	}
 }
